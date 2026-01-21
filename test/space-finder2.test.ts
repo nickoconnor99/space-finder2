03e1afbaed687809c0fccacc1cb192a74da7e442
nickoconnor99 <nickoconnor@gmail.com>
2026-01-21 16:38:39 +0000

    first commit

---
+++ test/space-finder2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SpaceFinder2 from '../lib/space-finder2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/space-finder2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SpaceFinder2.SpaceFinder2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
